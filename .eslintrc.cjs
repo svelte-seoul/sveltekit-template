4595f1e2e5c59a53e2b65a27f733a20efd2815c7
DevYuns <ydhun93@gmail.com>
2022-04-13 20:39:59 +0900

    Install @dooboo/eslint-config-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,7 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{files: ['*.svelte'], processor: 'svelte3/svelte3'}],
-  settings: {
-    'svelte3/typescript': () => require('typescript'),
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
+  extends: '@dooboo/eslint-config-svelte',
+  rules: {
+    '@typescript-eslint/no-unsafe-call': 'off',
   },
 };
